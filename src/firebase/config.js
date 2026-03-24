78afb22c4787520337ab8244d8d3929ed9005990
Gustavo <gustavo@global3g.com>
2026-03-23 17:58:35 -0700

    Security: move Firebase keys to env vars, add .env to gitignore

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBWN-o1zMqZ5DKj3zTRkHi0f9LSIWr-F58",
-  authDomain: "guerrero-65fa4.firebaseapp.com",
-  projectId: "guerrero-65fa4",
-  storageBucket: "guerrero-65fa4.firebasestorage.app",
-  messagingSenderId: "990889658118",
-  appId: "1:990889658118:web:88cd42daa7d4f41187c087"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyBWN-o1zMqZ5DKj3zTRkHi0f9LSIWr-F58",
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "guerrero-65fa4.firebaseapp.com",
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "guerrero-65fa4",
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "guerrero-65fa4.firebasestorage.app",
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "990889658118",
+  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:990889658118:web:88cd42daa7d4f41187c087"
 }
 
 const app = initializeApp(firebaseConfig)
